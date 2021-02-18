e03c7b9484715b093183b280c6b7dae49799e396
michaeltuijp <705542@cognizant.com>
2021-02-18 12:50:19 +0100

    Updated repository

--- setup.sh
+++ setup.sh
@@ -6,8 +6,4 @@ port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
 \n\
-git clone https://github.com/ourownstory/neural_prophet.git\n\
-cd neural_prophet\n\
-pip install .\n\
-\n\
 " > ~/.streamlit/config.toml
